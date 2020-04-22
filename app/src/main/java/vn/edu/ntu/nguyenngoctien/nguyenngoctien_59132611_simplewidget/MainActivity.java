95eeb377a1716c6184cbb96bcf7f9003c1aeeb7e
ngoct <tien.nn.59cntt@ntu.educom>
2020-04-23 00:33:43 +0700

    NguyenNgocTien_59132611_SimpleWidget

---
+++ app/src/main/java/vn/edu/ntu/nguyenngoctien/nguyenngoctien_59132611_simplewidget/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.nguyenngoctien.nguyenngoctien_59132611_simplewidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
